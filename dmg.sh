7272fc230f0964a1a48c0da41711a7f7facca627
erendabn <eren.daban@hotmail.com>
2026-08-12 09:13:08 +0300

    fix:macos build errors

---
+++ dmg.sh
@@ -0,0 +1,16 @@
+cd /Volumes/HIKSEMI/coding/CoPrintSlicer
+
+# 1. Geçici klasör oluştur
+mkdir -p dmg_temp
+
+# 2. .app'i kopyala
+cp -R build/arm64/src/Release/CoPrintSlicer.app dmg_temp/
+
+# 3. Applications'a symlink ekle
+ln -s /Applications dmg_temp/Applications
+
+# 4. DMG oluştur
+hdiutil create -volname "CoPrintSlicer" -srcfolder dmg_temp -ov -format UDZO CoPrintSlicer.dmg
+
+# 5. Temizlik
+rm -rf dmg_temp
